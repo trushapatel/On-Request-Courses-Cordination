88a1dfef80a59cbb2fa8c112bf65ce7dce62dd7a
trushapatel <trusha.be@gmail.com>
2023-06-07 18:42:35 +0530

    Update CourseService.java

--- OSR/src/main/java/com/ocr/service/CourseService.java
+++ OSR/src/main/java/com/ocr/service/CourseService.java
@@ -4,9 +4,7 @@ import com.ocr.domain.Course;
 
 import java.util.List;
 
-/**
- * Created by Ankit on 28-01-2017.
- */
+
 public interface CourseService {
     public void save(Course t);
     public Course get(Integer id);
